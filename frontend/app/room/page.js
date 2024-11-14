27b788cab72c918c1d09f22aaa90cd20732ebbf8
bob-th <67490142+bob-th@users.noreply.github.com>
2024-11-14 11:54:01 -0500

    delete backend folder

--- frontend/app/room/page.js
+++ frontend/app/room/page.js
@@ -1,3 +1,25 @@
+'use client'
+
+import { useEffect } from "react";
+import RTCContainer from "../Components/RTCContainer/RTCContainer";
+
 export default function RoomPage() {
-    
+
+    // useEffect( () => {
+    //     fetch('/api/room', {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify({ roomName: 'New Room' }),
+    //       });          
+    // },[]);
+
+
+    return (
+        <div className="RoomPageContainer">
+            <div className="HostElements">
+                <RTCContainer>
+                </RTCContainer>
+            </div>
+        </div>
+    );
 }
