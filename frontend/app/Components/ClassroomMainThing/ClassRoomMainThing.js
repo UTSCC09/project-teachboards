88a947312d16ba9639e9aa5ae6a1f0db0ba9514c
Peter Yoo <petersyoo23@gmail.com>
2024-11-27 01:28:10 -0500

    Making the landing page

---
+++ frontend/app/Components/ClassroomMainThing/ClassRoomMainThing.js
@@ -0,0 +1,96 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import "./ClassRoomMainPage.css";
+import Link from "next/link";
+import { useAuth } from "../Content/AuthContext.js";
+
+export default function ClassRoomMainPage() {
+    const { user,checkAuthStatus} = useAuth();
+    const [className, setClassName] = useState("");
+    const [classrooms, setClassrooms] = useState([]);
+
+    const handleAddClassroom = async (e) => {
+        e.preventDefault();
+        const id = user.id;
+        const pack = { id, className };
+
+        try {
+            const response = await fetch("/api/classroom", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(pack),
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                console.log("Could not create classroom");
+                return;
+            }
+            console.log("Classroom created:", data.classRoomId);
+            setClassrooms([data,...classrooms]);
+            setClassName(""); 
+            e.target.reset();
+        } catch (error) {
+            console.error("Error creating classroom:", error);
+        }
+    };
+
+    const handleGetClassroom = async () =>{
+        const id = user.id;
+        if (id != null){
+            try{    
+                const response = await fetch(`/api/classroom/${id}`, {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" },
+                });                
+                const data = await response.json();
+                if (!response.ok){
+                    console.log("could not get clasrooms");
+                    setClassrooms([]);
+                    return;
+                }
+                setClassrooms(data);
+            }
+            catch(error){
+                console.error("error gettinv classroom", error);
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (user) {
+            handleGetClassroom();
+        }
+    }, [user,checkAuthStatus]); 
+
+    return (
+        <div className="HomePageContainer">
+            <div className="welcome">{user ? `Welcome, ${user.firstName}` : "Please Login"}</div>
+            
+            {user && (
+                <form onSubmit={handleAddClassroom} className="AddClassroomForm">
+                    <input
+                        type="text"
+                        placeholder="Classroom Name"
+                        value={className}
+                        onChange={(e) => setClassName(e.target.value)}
+                        required
+                    />
+                    <button type="submit" className="AddClassroomButton">Add Classroom</button>
+                </form>
+            )}
+            
+            {user && (
+                <div className="ClassRoomComponentHolder">
+                {classrooms.map((classroom) => (
+                    <Link key={classroom.classRoomID} href={{pathname: `/classroom/${classroom.classRoomID}`, 
+                        query:{name: classroom.className}}}>
+                    <div key={classroom.classRoomID} id={classroom.classRoomID} className="ClassroomBox">
+                        {classroom.className}
+                    </div>
+                    </Link>
+                ))}
+                </div>
+            )}
+        </div>
+    );
+}
--- frontend/app/Components/Landing/Landing.js
+++ frontend/app/Components/Landing/Landing.js
@@ -1,15 +1,31 @@
+"use client";
+
 import "./Landing.css";
-
-
+import {useAuth} from "../Content/AuthContext.js";
+import {gsap} from "gsap";
+import {React, useEffect} from "react";
+import Link from "next/link";
 
 export default function Landing(){
-
-
-
-
+    const {user} = useAuth();
+    useEffect(() => {
+        const timeline = gsap.timeline({repeat: -1, repeatDelay: 0.5, });
+        timeline.from(".ETeachboards span",{y:0, opacity:0, rotateY:0});
+        timeline.to(".ETeachboards span",{y: -15, opacity: 1, rotateY: 360, stagger: 0.2, duration: 1, ease: "sine.inOut"});
+        timeline.to(".ETeachboards span",{y: 0, opacity: 0.1,rotateY:0, stagger:0.2, duration:1, ease:"sine.inOut"});
+        timeline.to(".ETeachboards span",{y:0, opacity:0, rotateY:0});
+    }, []);
     return(
         <div className="Entire">
-            
+            <p className="ETeachboards">
+                <span>T</span><span>e</span><span>a</span><span>c</span><span>h</span><span>b</span><span>o</span><span>a</span><span>r</span><span>d</span><span>s</span>
+            </p>
+            {!user && <div className="ENotLogin">
+                <Link href="/login"><p className="ELoginButton">Login</p></Link>
+            </div>}
+            {user && <div className = "ELoggedIn">
+                <Link href="/home"><p className="ELoginButton">Dive In</p></Link>
+            </div>}
         </div>
     )
 };
